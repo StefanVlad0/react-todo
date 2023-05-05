8cbf3d30d33f3f716604836c8e1a07d4329d698c
Stefan Vlad <72700839+StefanVlad0@users.noreply.github.com>
2023-05-05 14:59:04 +0300

    start

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,37 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import CreateTask from '../modals/createTask';
-import Card from './Card';
 
 const TodoList = () => {
-    const [modal, setModal] = useState(false);
-    const [taskList, setTaskList] = useState([])
-    useEffect(() => {
-        let arr = localStorage.getItem("taskList")
-        
-        if(arr){
-            let obj = JSON.parse(arr)
-            setTaskList(obj)
-        }
-    }, [])
-    const toggle = () => {setModal(!modal);}
-    const saveTask = (taskObj) => {
-        let tempList = taskList
-        tempList.push(taskObj)
-        localStorage.setItem("taskList", JSON.stringify(tempList))
-        setTaskList(tempList)
-        setModal(false)
-    }
+    const handleClick = () => {
+        console.log('Button clicked');
+      };
     return (
     <> 
         <div className="header text-center">
             <h3>Todo List</h3>
-            <button className= "btn btn-primary mt-2" onClick = {() => setModal(true)}>Create Task</button>
-            
+            <button className= "btn btn-primary mt-2" onClick={handleClick}>Create Task</button>
         </div>
-        <div className = "task-container">
-            {taskList && taskList.map((obj, index) => <Card taskObj = {obj} index = {index} />)}
-        </div>
-        <CreateTask toggle = {toggle} modal = {modal} save ={saveTask} />
     </>
     );
 };
